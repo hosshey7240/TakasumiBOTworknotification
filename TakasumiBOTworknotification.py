d9cd50a437be8303b0fcfb9677761816c7a751f6
nemu-a1021 <123063756+nemu-a1021@users.noreply.github.com>
2024-09-24 16:06:59 +0900

    変更部分は見て察してください
    Signed-off-by: nemu-a1021 <123063756+nemu-a1021@users.noreply.github.com>

--- TakasumiBOTworknotification.py
+++ TakasumiBOTworknotification.py
@@ -13,11 +13,11 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author.id == 981314695543783484 and len(message.embeds) = 1 and "コイン手に入れました" in message.embeds[0].author.name:
-        embed = discord.Embed(title="TakasumiBOT work通知",description="workを受信しました。\n20分後に通知します。",color=discord.Color.brand_green())
+    if message.author.id == 981314695543783484 and len(message.embeds) == 1 and "コイン手に入れました" in message.embeds[0].author.name:
+        embed = discord.Embed(title="TakasumiBOT work通知", description="workを受信しました。\n20分後に通知します。", color=discord.Color.brand_green())
         await message.reply(embed=embed)
         await asyncio.sleep(1200)
-        embed2 = discord.Embed(title="TakasumiBOT work通知",description="workの時間です\n</work:1132868147519692871>でお金を得ましょう",color=discord.Color.brand_green())
+        embed2 = discord.Embed(title="TakasumiBOT work通知", description="workの時間です\n</work:1132868147519692871>でお金を得ましょう", color=discord.Color.brand_green())
         await message.channel.send(embed=embed2)
-            
+
 client.run(TOKEN)
